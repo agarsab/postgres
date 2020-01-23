dc93116ccce8323372fa8e5810c08b7ad95790d5
agarsab <agarsab@gmail.com>
2020-01-23 08:40:33 +0000

    Split dump
    Create a single file for each database
    Create a file "globals" for global objects

--- postgresql_backup.sh
+++ postgresql_backup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+#================================================================
+# AUTHOR        Angel Garcia-Galan (angelgs@gmail.com)
+# COPYRIGHT     Cabildo de Tenerife (http://www.tenerife.es)
+# LICENSE       European Union Public Licence (EUPL) (https://joinup.ec.europa.eu/collection/eupl/)
+# SOURCE        https://github.com/agarsab/postgres
 #
 # FUNCTION      Makes a full backup (dump) of all PostgreSQL databases
 # NOTES         The script has to be run with postgres user/owner
 #               Tested in PostgreSQL 9.6 over CentOS 7.4
+#
 # ARGUMENTS     $1 = Path to store dump and log files
 #               $2 = Number of days to preserve dumps
 
 function print_log {
-        DATE_HOUR=`date '+%Y-%m-%d %H:%M'`
-        echo $DATE_HOUR" "$1
+        NOW=`date '+%Y-%m-%d %H:%M:%S'`
+        echo $NOW" "$1" "$2
 }
 
 EXIT_OK=0
 EXIT_ERROR=1
-DATE_HOUR=`date '+%Y-%m-%d_%H-%M'`
+TODAY=`date '+%Y-%m-%d'`
 
 if [ "$1" == "" ] || [ "$2" == "" ]; then
         print_log "ERROR: missing arguments."
@@ -23,12 +29,12 @@ fi
 
 BACKUP_HOME=$1
 BACKUP_DAYS=$2
-BACKUP_CMD="/bin/pg_dumpall"
-BACKUP_DUMP=$BACKUP_HOME"/"$DATE_HOUR"_"`hostname`".dmp"
-BACKUP_LOG=$BACKUP_HOME"/"$DATE_HOUR"_"`hostname`".log"
+BACKUP_DBS=`psql -lt | grep -v : | cut -d \| -f 1 | grep -v template | grep -v -e '^\s*$' | sed -e 's/  *$//'|  tr '\n' ' '`
+BACKUP_LOG=$BACKUP_HOME"/"$TODAY"_"`hostname`".log"
+BACKUP_CMD="/bin/pg_dump"
 
 if [[ ! $BACKUP_DAYS =~ ^[0-9]+$ ]] || [[ ! $BACKUP_DAYS -gt 1 ]]; then
-        print_log "ERROR: number of days: "$BACKUP_DAYS
+        print_log "ERROR: number of days:" $BACKUP_DAYS
         exit $EXIT_ERROR
 fi
 
@@ -38,39 +44,67 @@ if [ ! $PGDATA ]; then
 fi
 
 if [ ! -d $PGDATA ]; then
-        print_log "ERROR: data path not found: "$PGDATA
+        print_log "ERROR: data path not found:" $PGDATA
         exit $EXIT_ERROR
 fi
 
 if [ ! -w $BACKUP_HOME ]; then
-        print_log "ERROR: backup path not writable: "$BACKUP_HOME
+        print_log "ERROR: backup path not writable:" $BACKUP_HOME
         exit $EXIT_ERROR
 fi
 
 if [ ! -f $BACKUP_CMD ]; then
-        print_log "ERROR: backup command not found: "$BACKUP_CMD
+        print_log "ERROR: backup command not found:" $BACKUP_CMD
         exit $EXIT_ERROR
 fi
 
-print_log "INFO: Starting dump..." >> $BACKUP_LOG
-$BACKUP_CMD > $BACKUP_DUMP
-print_log "`ls -lh $BACKUP_DUMP`" >> $BACKUP_LOG
-print_log "INFO: Dump finished." >> $BACKUP_LOG
+if [ -z "$BACKUP_DBS" ]; then
+        print_log "ERROR: empty database list"
+        exit $EXIT_ERROR
+fi
 
-print_log "INFO: Compressing dump..." >> $BACKUP_LOG
-nice gzip $BACKUP_DUMP
-print_log "`ls -lh $BACKUP_DUMP.gz`" >> $BACKUP_LOG
-print_log "INFO: Compression finished." >> $BACKUP_LOG
+BACKUP_DB="globals"
+BACKUP_SQL=$BACKUP_HOME"/"$TODAY"_"`hostname`"_"$BACKUP_DB".sql"
+print_log "INFO: Starting backup database" $BACKUP_DB >> $BACKUP_LOG
+BACKUP_CMD="pg_dumpall --globals-only"
+$BACKUP_CMD > $BACKUP_SQL 2>> $BACKUP_LOG
+print_log "`ls -lh $BACKUP_SQL`" >> $BACKUP_LOG
+BACKUP_RESULT=`tail -5 "$BACKUP_SQL" | grep 'database dump complete'`
+if [ -z "BACKUP_RESULT" ]; then
+        print_log "ERROR: Backup failed in database" $BACKUP_DB >> $BACKUP_LOG
+else
+        print_log "INFO: Completed backup database" $BACKUP_DB >> $BACKUP_LOG
+fi
+print_log "INFO: Compressing file "$BACKUP_DB"..." >> $BACKUP_LOG
+nice gzip $BACKUP_SQL
+print_log "`ls -lh $BACKUP_SQL.gz`" >> $BACKUP_LOG
+
+for BACKUP_DB in $BACKUP_DBS; do
+        BACKUP_SQL=$BACKUP_HOME"/"$TODAY"_"`hostname`"_"$BACKUP_DB".sql"
+        print_log "INFO: Starting backup database" $BACKUP_DB >> $BACKUP_LOG
+        BACKUP_CMD="pg_dump --format=p "$BACKUP_DB
+        $BACKUP_CMD > $BACKUP_SQL 2>> $BACKUP_LOG
+        print_log "`ls -lh $BACKUP_SQL`" >> $BACKUP_LOG
+        BACKUP_RESULT=`tail -5 "$BACKUP_SQL" | grep 'database dump complete'`
+        if [ -z "BACKUP_RESULT" ]; then
+                print_log "ERROR: Backup failed in database" $BACKUP_DB >> $BACKUP_LOG
+        else
+                print_log "INFO: Completed backup database" $BACKUP_DB >> $BACKUP_LOG
+        fi
+        print_log "INFO: Compressing file "$BACKUP_DB"..." >> $BACKUP_LOG
+        nice gzip $BACKUP_SQL
+        print_log "`ls -lh $BACKUP_SQL.gz`" >> $BACKUP_LOG
+done
 
 print_log "INFO: Removing old dumps and logs..." >> $BACKUP_LOG
-OLD_FILES=`find $BACKUP_HOME -iname '20*.dmp.gz' -mtime +$BACKUP_DAYS`
+OLD_FILES=`find $BACKUP_HOME -iname '20*.sql.gz' -mtime +$BACKUP_DAYS`
 for FILE in $OLD_FILES; do
-        print_log "INFO: Removing file "$FILE >> $BACKUP_LOG
+        print_log "INFO: Removing file" $FILE >> $BACKUP_LOG
         rm $FILE
 done
 OLD_FILES=`find $BACKUP_HOME -iname '20*.log' -mtime +$BACKUP_DAYS`
 for FILE in $OLD_FILES; do
-        print_log "INFO: Removing file "$FILE >> $BACKUP_LOG
+        print_log "INFO: Removing file" $FILE >> $BACKUP_LOG
         rm $FILE
 done
 
